6e1b2f2e14b69771e614a00a08a69889dffcfc15
Leon Jamison <leonjamison@icloud.com>
2020-01-01 20:12:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,5 +29,5 @@ def using_concat(my_favorite_things,more_favs)
 end
 
 def using_insert(list_of_programming_languages,another_language)
-  list_of_programming_languages.insert(new_array[4])
+  list_of_programming_languages.insert(4,"Python")
 end
